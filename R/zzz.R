e4501b862f4932d07ab97ec2b0a213b1edd2b020
Hugh Warden <hugh.warden@outlook.com>
2023-03-25 23:45:51 +0000

    Adding statup message

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "==AcademicThemes================================\n",
+    "Please be mindful of the effects of your choice\n",
+    "of colour palette on people who are colour blind\n",
+    "================================================"
+  )
+}
